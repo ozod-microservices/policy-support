e271d7ea8b20e178aa810604874c3f51bb7a2395
NIKHIL RAJ <nikhil@omniesolutions.com>
2024-02-28 13:44:20 +0530

    first

---
+++ src/main/java/com/policy/policysupport/PolicySupportApplication.java
@@ -0,0 +1,13 @@
+package com.policy.policysupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicySupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicySupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/policy/policysupport/PolicySupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policy.policysupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicySupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
